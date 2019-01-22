b562e86f119700990ea72b162b8ac0e3f3467b09
Jeffrey H. Chen <jeffreyhc@berkeley.edu>
2019-01-22 10:29:28 -0800

    Boilerplate (#1)
    * added more node modules
    
    * added graphQL morgan tracker, and react is running, will need to update the files
    
    * added stuff
    
    * added controllers
    
    * graphql kind of decomped, need to add more graphql-tools later

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                This is going to be a pokedex
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,39 @@
+global.__basedir = __dirname
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 4000 
+const morgan = require('morgan')
+const graphqlHTTP = require('express-graphql');
+const healthCheckSchema = require('./schemas/healthCheck')
+const healthCheckResolver = require('./resolvers/healthCheck/healthCheck')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('client/dist')) 
+
+/**
+ * This is for the express person of capturing the graphql query when we make an API call through a specific route
+ */
+morgan.token('graphql-query', (req) => {
+  const {query, variables, operationName} = req.body;
+  return `GRAPHQL: \nOperation Name: ${operationName} \nQuery: ${query} \nVariables: ${JSON.stringify(variables)}`;
+});
+
+app.use(morgan(':method :url :status :res[content-length] :graphql-query - :response-time ms'))
+
+app.get('/healthCheck', (req, res) => {
+  res.status(200).send('health check OK')
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema: healthCheckSchema,
+  rootValue: healthCheckResolver,
+  graphiql: true,
+}));
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`)
+});
+
