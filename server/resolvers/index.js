4354af8f51a4af4996b6938c191bbd10867474d5
Jeff Chen <jeffchen@paypal.com>
2019-02-25 16:54:35 -0800

    working

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,6 +1,8 @@
-import path from 'path';
-import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
+const path = require('path');
+const mergeGraphqlSchemes = require('merge-graphql-schemas')
+const fileLoader = mergeGraphqlSchemes.fileLoader
+const mergeResolvers = mergeGraphqlSchemes.mergeResolvers
 
 const resolversArray = fileLoader(path.join(__dirname, "./**/*.resolvers.*"));
 const merged = mergeResolvers(resolversArray);
-export default merged
+module.exports = merged
